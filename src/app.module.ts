2d25babf11c6ffd163cecc02faf060a3a3ec34c3
Seongtae <stae1102@gmail.com>
2022-10-31 20:15:22 +0900

    feat: 추가 MySqlConfigModule, MySqlConfigService
    기존에 dotenv를 통해서 TypeOrm의 옵션을 제공했던 방식에서 벗어나서
    NestJS에서 권고하는 ConfigModule 방식으로 변경했습니다.
    
    Fixes: #16

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,19 @@ import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoggerMiddleware } from './middlewares/logger.middleware';
-import ormconfig from '../ormconfig';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { MySqlConfigModule } from './config/database/config.module';
+import { MySqlConfigService } from './config/database/config.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(ormconfig)],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [MySqlConfigModule],
+      useExisting: MySqlConfigService,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
