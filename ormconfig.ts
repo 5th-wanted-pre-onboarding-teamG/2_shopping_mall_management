0c7abb8a5a5260ef1a2ccb54e70396919bebcdfb
eunkangchoi <ekk12mv2@gmail.com>
2022-10-31 17:13:25 +0900

    chore: config 임포트문 수정

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,5 +1,5 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
-import dotenv from 'dotenv';
+import * as dotenv from 'dotenv';
 
 dotenv.config();
 const config: TypeOrmModuleOptions = {
@@ -18,4 +18,4 @@ const config: TypeOrmModuleOptions = {
   keepConnectionAlive: true,
 };
 
-export = config;
+export default config;
