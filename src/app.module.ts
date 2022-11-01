ec3ba9c48e4af9fe809c588951a5c4af13598185
Seongtae <stae1102@gmail.com>
2022-11-02 02:07:56 +0900

    chore: 추가 orders resource
    orders controller, service, module을 생성하였습니다.
    추가적으로, app.module.ts에도 의존성 주입했습니다.
    
    Fixes: #39

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { MySqlConfigModule } from './config/database/config.module';
 import { MySqlConfigService } from './config/database/config.service';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
+import { OrdersModule } from './orders/orders.module';
 
 @Module({
   imports: [
@@ -16,6 +17,7 @@ import { AuthModule } from './auth/auth.module';
       useClass: MySqlConfigService,
     }),
     AuthModule,
+    OrdersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
