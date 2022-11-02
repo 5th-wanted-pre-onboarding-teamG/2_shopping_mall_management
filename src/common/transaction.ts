6956ab8fb3a196466c32423f73cc3ffb03900d75
MisterRuby <ruby8700@naver.com>
2022-11-02 17:06:57 +0900

    chore: transaction.ts 파일 내 console.log 제거

--- src/common/transaction.ts
+++ src/common/transaction.ts
@@ -17,26 +17,22 @@ export const wrapTransaction = async (
   await queryRunner.startTransaction(isolationLevel);
   const entityManager = queryRunner.manager;
 
-  console.log('Transaction Start!');
   try {
     // 비즈니스 로직 실행
     const execute = await logic(entityManager);
 
     // sql 커밋
     await queryRunner.commitTransaction();
-    console.log('Transaction Commit!');
 
     return execute;
   } catch (error) {
     // 오류 발생시 롤백 처리
     await queryRunner.rollbackTransaction();
-    console.log('Transaction Rollback!');
 
     throw new SQLException();
   } finally {
     // 커넥션 연결 해제
     await queryRunner.release();
-    console.log('Transaction Release & End');
   }
 };
 
