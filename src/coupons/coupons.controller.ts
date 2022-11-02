dc168bbcb9ff88cc9d0c5252acc2c991671c5628
eunkangchoi <ekk12mv2@gmail.com>
2022-11-02 21:14:25 +0900

    feat: 쿠폰 목록 조회 API 생성
    - 운영자(user.rank=Manager) 만 조회가능
    - couponType 를 검색가능
    - couponType enum에 해당하지 않은 쿠폰타입은 검색할 수 없게함.(badRequest 에러발생)

--- src/coupons/coupons.controller.ts
+++ src/coupons/coupons.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query, UseGuards } from '@nestjs/common';
 import { User } from 'src/auth/auth.decorator';
 import { Users } from 'src/entities/Users';
 import { AuthenticatedGuard } from '../auth/auth.guard';
@@ -10,14 +10,26 @@ export class CouponsController {
   constructor(private readonly couponsService: CouponsService) {}
 
   /**
-   * @api POST /api/coupons/
-   * @description: 운영자가 새로운 쿠폰을 등록합니다.
+   * @url [POST] /api/coupons/
+   * @description: 운영자가 새로운 쿠폰을 생성합니다.
    * @successCode: 201
-   * @errorCode: 500
+   * @errorCode: 400, 500
    */
   @UseGuards(AuthenticatedGuard)
   @Post('')
   async createCoupons(@User() user: Users, @Body() createCouponDto: CreateCouponDto) {
     return await this.couponsService.createCoupon(user, createCouponDto);
+  }
+
+  /**
+   * @url [GET] /api/coupons
+   * @query: couponType 쿠폰타입
+   * description: 운영자는 생성한 모든 쿠폰들을 조회할 수 있습니다.
+   * @successCode: 200
+   */
+  @UseGuards(AuthenticatedGuard)
+  @Get()
+  async getAllCoupons(@User() user: Users, @Query('couponType') couponType?: string) {
+    return this.couponsService.getAllCoupons(user, couponType);
   }
 }
