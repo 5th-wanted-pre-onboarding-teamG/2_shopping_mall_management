0fe45308141f2aa1c610935248a714eba35de473
Kang Jinhee <20wlsgml@gmail.com>
2022-11-02 17:35:41 +0900

    feat: 국적 등록 dto 추가

---
+++ src/countries/dto/create-countries.dto.ts
@@ -0,0 +1,11 @@
+import { IsNotEmpty } from 'class-validator';
+export class CreateCountriesDto {
+  @IsNotEmpty()
+  countryCode: string;
+
+  @IsNotEmpty()
+  dCode: string;
+
+  @IsNotEmpty()
+  name: string;
+}
