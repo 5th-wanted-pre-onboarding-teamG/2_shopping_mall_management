b93d09e8f28ad37eb5ea25de4b6d38b57cf5fc5f
nuobasic <nuobasic@naver.com>
2022-11-02 16:40:05 +0900

    add: products 생성

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
-
+import { Controller, UseGuards, Post, Body } from '@nestjs/common';
+import { ProductsService } from './products.service';
+import { AuthenticatedGuard } from 'src/auth/auth.guard';
+import { CreateProductDto } from './dto/create-product.dto';
+import { Users } from 'src/entities/Users';
+import { User } from 'src/auth/auth.decorator';
 @Controller('products')
-export class ProductsController {}
+export class ProductsController {
+  constructor(private readonly productsServics: ProductsService) {}
+
+  /**
+   * @url POST 'api/products'
+   * @Body createProductsDto: 게시물 생성시 필요한 정보 입니다.{제목, 가격, 제고 }
+   * @User user:현재 로그인 된 유저를 나타냅니다.
+   * @description 유저가 상품을 생성합니다.
+   * @returns 상품 생성
+   */
+  @UseGuards(AuthenticatedGuard)
+  @Post()
+  async createProduct(@Body() createProductsDto: CreateProductDto, @User() user: Users) {
+    return await this.productsServics.createProducts(createProductsDto, user);
+  }
+}
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, ForbiddenException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Products } from 'src/entities/Products';
+import { Repository } from 'typeorm';
+import { CreateProductDto } from './dto/create-product.dto';
+import { Users } from 'src/entities/Users';
+import { UserRank } from 'src/entities/enums/userRank';
 
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+  constructor(
+    @InjectRepository(Products)
+    private readonly porductsRepository: Repository<Products>,
+  ) {}
+  async createProducts(createProductsDto: CreateProductDto, user: Users) {
+    const product = new Products();
+    // 유저의 rank가 NORMAL일 경우 접근을 할 수 없습니다.
+    if (user.rank === UserRank.NORMAL) {
+      throw new ForbiddenException('상품 생성 권한이 없습니다.');
+    }
+    product.name = createProductsDto.name;
+    product.price = createProductsDto.price;
+    product.stock = createProductsDto.stock;
+    product.Author = user;
+    //상품  저장
+    const saveProdut = await this.porductsRepository.save(product);
+    return saveProdut;
+  }
+}
