55d8e105a0365622de445dc62015f60f7eef3aea
eunkangchoi <ekk12mv2@gmail.com>
2022-11-02 13:41:48 +0900

    feat: 쿠폰모델 변경

--- src/entities/Coupons.ts
+++ src/entities/Coupons.ts
@@ -1,7 +1,7 @@
 import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 import { DateColumns } from './embeddeds/dateColumns';
 import { CouponType } from './enums/couponType';
-import { UserCoupons } from './UserCoupons';
+import { OwnedCoupons } from './OwnedCoupons';
 
 @Entity({ schema: 'product_shopping', name: 'coupons' })
 export class Coupons {
@@ -11,15 +11,37 @@ export class Coupons {
   @Column()
   name: string;
 
+  /**
+   * 쿠폰 타입
+   */
   @Column({ type: 'enum', name: 'couponType', enum: CouponType })
   couponType: CouponType;
 
+  /**
+   * 쿠폰할인 가격
+   * 퍼센트할인(%): 최소값 1% ~ 최댓값 100%
+   * 정액제(₩): 최소값 100원
+   */
   @Column()
-  sale: number;
+  salePrice: number;
 
+  /**
+   * 유효기간
+   */
+  @Column()
+  validPeriod: number;
+
+  /**
+   * 데이터 생성(createAt)/수정(updateAt)/삭제(deleteAt) 일자입니다.
+   * 모든 엔티티에 공통적으로 적용되는 요소입니다.
+   */
   @Column(() => DateColumns, { prefix: false })
   dateColumns: DateColumns;
 
-  @OneToMany(() => UserCoupons, (userCoupons) => userCoupons.coupon)
-  userCoupons: UserCoupons[];
+  /**
+   * 사용자:쿠폰 = 1:N
+   * 사용자가 보유한 쿠폰들을 의미합니다.
+   */
+  @OneToMany(() => OwnedCoupons, (ownedCoupons) => ownedCoupons.coupon)
+  ownedCoupons: OwnedCoupons[];
 }
