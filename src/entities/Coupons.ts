bca24b9433c645747d125bbefc7579ed91e96319
Seongtae <stae1102@gmail.com>
2022-11-03 11:02:31 +0900

    update: 엔티티 컬럼명 변경에 따른 쿼리 및 변수명 변경

--- src/entities/Coupons.ts
+++ src/entities/Coupons.ts
@@ -8,33 +8,18 @@ export class Coupons {
   @PrimaryGeneratedColumn({ type: 'int', name: 'couponId' })
   couponId: number;
 
-  @Column({ comment: '쿠폰 이름' })
+  @Column()
   name: string;
 
-  /**
-   * 쿠폰 타입
-   */
-  @Column({ type: 'enum', name: 'couponType', enum: CouponType, comment: '쿠폰타입' })
+  @Column({ type: 'enum', name: 'couponType', enum: CouponType })
   couponType: CouponType;
 
-  /**
-   * 쿠폰할인 가격
-   * 퍼센트할인(%): 최소값 1% ~ 최댓값 100%
-   * 정액제(₩): 최소값 1000원
-   */
-  @Column({ comment: '할인 가격(배송비, 퍼센트할인: % / 정액제: ₩)' })
-  discountedPrice: number;
+  @Column()
+  discount: number;
 
-  /**
-   * 유효기간
-   */
-  @Column({ comment: '쿠폰 유효기간: 하루(일) 단위' })
+  @Column()
   validPeriod: number;
 
-  /**
-   * 데이터 생성(createAt)/수정(updateAt)/삭제(deleteAt) 일자입니다.
-   * 모든 엔티티에 공통적으로 적용되는 요소입니다.
-   */
   @Column(() => DateColumns, { prefix: false })
   dateColumns: DateColumns;
 
--- src/payments/payments.service.ts
+++ src/payments/payments.service.ts
@@ -76,21 +76,15 @@ export class PaymentsService {
       const productPrice = existsOrder.Product?.price;
       const deliveryPrice = existsOrder.DeliveryCost?.price;
       const countryCode = existsOrder.DeliveryCost?.Country?.countryCode;
-      const { couponType, discountedPrice } = ownedCoupons.Coupon;
+      const { couponType, discount } = ownedCoupons.Coupon;
       const totalProductPrice = productPrice * quantity;
-      let paymentSalePrice = calculateSalePrice(
-        totalProductPrice,
-        deliveryPrice,
-        couponType,
-        discountedPrice,
-        countryCode,
-      );
+      let paymentSalePrice = calculateSalePrice(totalProductPrice, deliveryPrice, couponType, discount, countryCode);
       let paymentPrice = calculatePaymentPrice(totalProductPrice, deliveryPrice, paymentSalePrice, countryCode);
 
       // 결제 정보 등록
       await entityManager.getRepository(Payments).insert({
         paymentState: PaymentState.COMPLETE,
-        salePrice: paymentSalePrice,
+        discountedPrice: paymentSalePrice,
         paymentPrice,
         Order: existsOrder,
         User: user,
