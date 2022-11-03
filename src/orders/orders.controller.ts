1908caf205ed8a52bd98f0f7b4916835ffeb9ad1
Seongtae <stae1102@gmail.com>
2022-11-03 11:19:48 +0900

    feat: 추가 주문 - 주문하기(createOrder)
    주문하기 기능을 추가했습니다. 코드를 한 번에 작성해서 변경 사항을
    부분적으로 커밋을 날리지 못해 임포트 부분은 전체 다 커밋을 날렸습니다.
    다음에는 부분부분으로 커밋을 날리겠습니다.
    
    Fixes: #39

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,7 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, Query, UseGuards } from '@nestjs/common';
+import { Users } from 'src/entities/Users';
+import { CreateOrderDto } from './dto/create-order.dto';
 import { OrdersService } from './orders.service';
 
 @Controller('orders')
 export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
+
+  @Post()
+  createOrder(@User() user: Users, @Body() createOrderDto: CreateOrderDto) {
+    return this.ordersService.createOrder(user, createOrderDto);
+  }
+
 }
--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,4 +1,69 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Countries } from 'src/entities/Countries';
+import { DeliveryCosts } from 'src/entities/DeliveryCosts';
+import { OrderState } from 'src/entities/enums/orderState';
+import { Orders } from 'src/entities/Orders';
+import { Products } from 'src/entities/Products';
+import { Users } from 'src/entities/Users';
+import { DataSource, Repository } from 'typeorm';
+import { CreateOrderDto } from './dto/create-order.dto';
 
 @Injectable()
+export class OrdersService {
+  private take = 30;
+  constructor(
+    private readonly dataSource: DataSource,
+    @InjectRepository(Orders) private readonly ordersRepository: Repository<Orders>,
+    @InjectRepository(Users) private readonly usersRepository: Repository<Users>,
+  ) {}
+
+  async createOrder(user: Users, createOrderDto: CreateOrderDto) {
+    const queryRunner = this.dataSource.createQueryRunner();
+
+    // 트랜잭션 시작
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+
+    // 서비스 구현
+    try {
+      // 국가명
+      const [{ name }, { deliveryCostId }] = await Promise.all([
+        queryRunner.manager.getRepository(Countries).findOne({
+          where: { countryId: createOrderDto.countryId },
+        }),
+        queryRunner.manager.getRepository(DeliveryCosts).findOne({
+          where: { CountryId: createOrderDto.countryId, quantity: createOrderDto.quantity },
+        }),
+      ]);
+
+      // 주문 내역 작성
+      const [result, _] = await Promise.all([
+        await queryRunner.manager.getRepository(Orders).save({
+          UserId: user.userId,
+          ProductId: createOrderDto.productId,
+          DeliveryCostId: deliveryCostId,
+          orderState: OrderState.PAYMENT_WAITING,
+          quantity: createOrderDto.quantity,
+          address: { country: name, city: createOrderDto.city, zipcode: createOrderDto.zipCode },
+          recipientName: createOrderDto.name || '',
+          recipientPhone: createOrderDto.phone || '',
+        }),
+        await queryRunner.manager
+          .createQueryBuilder()
+          .update(Products)
+          .set({ stock: () => 'stock - 1' })
+          .where('productId = :productId', { productId: createOrderDto.productId })
+          .execute(),
+      ]);
+      await queryRunner.commitTransaction();
+      return result;
+    } catch (error) {
+      await queryRunner.rollbackTransaction();
+      throw new BadRequestException('주문 과정에서 오류가 발생했습니다.');
+    } finally {
+      await queryRunner.release();
+    }
+  }
+
 export class OrdersService {}
