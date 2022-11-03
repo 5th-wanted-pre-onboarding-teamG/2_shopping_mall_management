e588b39b265ce173973126e6dd5502cb557c32bb
eunkangchoi <ekk12mv2@gmail.com>
2022-11-03 14:38:34 +0900

    fix: Users 엔티티에 products 필드 추가코드 병합

--- src/entities/Users.ts
+++ src/entities/Users.ts
@@ -4,6 +4,7 @@ import { OwnedCoupons } from './OwnedCoupons';
 import { Orders } from './Orders';
 import { UserRank } from './enums/userRank';
 import { Payments } from './Payments';
+import { Products } from './Products';
 
 @Entity({ schema: 'product_shopping', name: 'users' })
 export class Users {
@@ -41,4 +42,7 @@ export class Users {
 
   @OneToMany(() => Payments, (payments) => payments.User)
   Payments: Payments[];
+
+  @OneToMany(() => Products, (products) => products.Author)
+  products: Products[];
 }
