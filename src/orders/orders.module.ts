17a6010b925ade588119968e80ba88fa91a4002d
Seongtae <stae1102@gmail.com>
2022-11-03 11:15:34 +0900

    feat: 추가 orders module
    주문 기능을 위해 orders module을 추가했으며, 필요한 레포지토리를
    임포트했습니다.
    
    Fixes: #39

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { OrdersController } from './orders.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Orders } from 'src/entities/Orders';
+import { Users } from 'src/entities/Users';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Users, Orders])],
   controllers: [OrdersController],
   providers: [OrdersService],
 })
