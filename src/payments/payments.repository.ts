1af7d7a0963df01935881616eaac090b22f5cd90
MisterRuby <ruby8700@naver.com>
2022-11-06 00:15:32 +0900

    refactor: 결제 모듈 리포지토리의 반환타입을 엔티티에서 Dto로 변경
    엔티티로 받을 경우 엔티티와 연관관계에 있는 다른 엔티티의 속성 값을 받아오지 못하여 Dto 로 결과값을 받도록 변경

--- src/payments/payments.repository.ts
+++ src/payments/payments.repository.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { Payments } from '../entities/Payments';
-import { EntityManager, Repository } from 'typeorm';
+import { DataSource, EntityManager, Repository } from 'typeorm';
 import { Orders } from '../entities/Orders';
 import { Products } from '../entities/Products';
 import { DeliveryCosts } from '../entities/DeliveryCosts';
@@ -12,15 +12,22 @@ import { Users } from '../entities/Users';
 import { ResultUserPayments } from './dto/result-user-payments.dto';
 import { SearchPayments } from './dto/search-payments';
 import { ResultPaymentsDto } from './dto/result-payments.dto';
+import { ResultExistsOrderDto } from './dto/result-existsOrder.dto';
+import { ResultExistsOwnedCouponDto } from './dto/result-existsOwnedCoupon.dto';
 
 @Injectable()
 export class PaymentsRepository extends Repository<Payments> {
+  constructor(private readonly dataSource: DataSource) {
+    const baseRepository = dataSource.getRepository<Payments>(Payments);
+    super(baseRepository.target, baseRepository.manager, baseRepository.queryRunner);
+  }
+
   /**
    * 결제 진행을 위한 주문 조회
    * @param entityManager 엔티티 매니저
    * @param orderId 주문 Id
    */
-  async getOrderById(entityManager: EntityManager, orderId: number): Promise<Orders> {
+  async getOrderById(entityManager: EntityManager, orderId: number): Promise<ResultExistsOrderDto> {
     return entityManager
       .getRepository(Orders)
       .createQueryBuilder('orders')
@@ -28,14 +35,14 @@ export class PaymentsRepository extends Repository<Payments> {
       .innerJoinAndSelect(DeliveryCosts, 'deliveryCosts')
       .innerJoinAndSelect(Countries, 'countries', 'deliveryCosts.countryId = countries.countryId')
       .select([
-        'orders.quantity',
+        'orders.quantity as quantity',
         'products.price as productPrice',
         'deliveryCosts.price as deliveryPrice',
-        'countries.countryCode',
+        'countries.countryCode as countryCode',
       ])
       .where('orders.orderId = :orderId', { orderId })
       .andWhere('orders.orderState = :orderState', { orderState: OrderState.PAYMENT_WAITING })
-      .getOne();
+      .getRawOne();
   }
 
   /**
@@ -43,14 +50,18 @@ export class PaymentsRepository extends Repository<Payments> {
    * @param entityManager 엔티티 매니저
    * @param ownedCouponId 결제에 사용할 회원의 보유 쿠폰 Id
    */
-  async getOwnedCouponById(entityManager: EntityManager, ownedCouponId: number): Promise<OwnedCoupons> {
+  async getOwnedCouponById(entityManager: EntityManager, ownedCouponId: number): Promise<ResultExistsOwnedCouponDto> {
     return await entityManager
       .getRepository(OwnedCoupons)
       .createQueryBuilder('ownedCoupons')
       .innerJoinAndSelect(Coupons, 'coupons')
-      .select(['ownedCoupons.ownedCouponId', 'coupons.couponType', 'coupons.discount'])
+      .select([
+        'ownedCoupons.ownedCouponId as ownedCouponId',
+        'coupons.couponType as couponType',
+        'coupons.discount as discount',
+      ])
       .where('ownedCoupons.ownedCouponId = :ownedCouponId', { ownedCouponId })
-      .getOne();
+      .getRawOne();
   }
 
   /**
--- src/payments/payments.service.ts
+++ src/payments/payments.service.ts
@@ -9,12 +9,14 @@ import { ResultUserPayments } from './dto/result-user-payments.dto';
 import { SearchPayments } from './dto/search-payments';
 import { PaymentState } from '../entities/enums/paymentState';
 import { OrderState } from '../entities/enums/orderState';
-import { calculatePaymentPrice, calculateSalePrice } from './payments.calculate';
+import { calculatePaymentPrice, calculateSalePrice, correctionDollar } from './payments.calculate';
 import { ResultPaymentsDto } from './dto/result-payments.dto';
 import { wrapTransaction } from '../common/transaction';
 import { OwnedCoupons } from '../entities/OwnedCoupons';
 import { OrderNotFoundException } from '../exception/orders.exception';
 import { PaymentsRepository } from './payments.repository';
+import { ResultExistsOrderDto } from './dto/result-existsOrder.dto';
+import { ResultExistsOwnedCouponDto } from './dto/result-existsOwnedCoupon.dto';
 
 @Injectable()
 export class PaymentsService {
@@ -38,7 +40,7 @@ export class PaymentsService {
     await wrapTransaction(this.dataSource, async (entityManager: EntityManager) => {
       const orderId = createPaymentDto.orderId;
       // 결제할 주문 확인
-      const existsOrder = await this.paymentsRepository.getOrderById(entityManager, orderId);
+      const existsOrder: ResultExistsOrderDto = await this.paymentsRepository.getOrderById(entityManager, orderId);
 
       if (!existsOrder) {
         throw new OrderNotFoundException();
@@ -46,23 +48,23 @@ export class PaymentsService {
 
       // 사용할 쿠폰 확인
       const ownedCouponId = createPaymentDto.ownedCouponId;
-      const ownedCoupons = await this.paymentsRepository.getOwnedCouponById(entityManager, ownedCouponId);
+      const ownedCoupons: ResultExistsOwnedCouponDto = await this.paymentsRepository.getOwnedCouponById(
+        entityManager,
+        ownedCouponId,
+      );
 
       // 결제 정보에 저장할 금액 게산
-      const quantity = existsOrder.quantity;
-      const productPrice = existsOrder.Product?.price;
-      const deliveryPrice = existsOrder.DeliveryCost?.price;
-      const countryCode = existsOrder.DeliveryCost?.Country?.countryCode;
-      const couponType = ownedCoupons?.Coupon?.couponType;
-      const discount = ownedCoupons?.Coupon?.discount;
-      // const { couponType = 0, discount = 0 } = ownedCoupons?.Coupon;
-      const totalProductPrice = productPrice * quantity;
-      const paymentSalePrice = calculateSalePrice(totalProductPrice, deliveryPrice, couponType, discount, countryCode);
-      const paymentPrice = calculatePaymentPrice(totalProductPrice, deliveryPrice, paymentSalePrice, countryCode);
+      const { quantity, productPrice, deliveryPrice, countryCode } = existsOrder;
+      const { couponType, discount } = ownedCoupons;
+      const totalProductPrice = correctionDollar(productPrice * quantity, countryCode);
+      const correctionDeliveryPrice = correctionDollar(deliveryPrice, countryCode);
+      const paymentSalePrice = calculateSalePrice(totalProductPrice, correctionDeliveryPrice, couponType, discount);
+      const paymentPrice = calculatePaymentPrice(totalProductPrice, correctionDeliveryPrice, paymentSalePrice);
 
       // 결제 정보 등록
       await entityManager.getRepository(Payments).insert({
         paymentState: PaymentState.COMPLETE,
+        orderPrice: totalProductPrice + correctionDeliveryPrice,
         discountedPrice: paymentSalePrice,
         paymentPrice,
         Order: existsOrder,
@@ -70,14 +72,22 @@ export class PaymentsService {
       });
 
       // 주문의 상태를 결제 완료로 변경
-      await entityManager.getRepository(Orders).update(orderId, {
-        orderState: OrderState.PAYMENT_COMPLETE,
-      });
+      await entityManager
+        .getRepository(Orders)
+        .createQueryBuilder()
+        .update(Orders)
+        .set({ orderState: OrderState.PAYMENT_COMPLETE })
+        .where('orderId = :orderId', { orderId })
+        .execute();
 
       // 쿠폰을 사용 상태로 변경
-      await entityManager.getRepository(OwnedCoupons).update(ownedCoupons.ownedCouponId, {
-        Order: existsOrder,
-      });
+      await entityManager
+        .getRepository(OwnedCoupons)
+        .createQueryBuilder()
+        .update(OwnedCoupons)
+        .set({ OrderId: orderId, usedDate: new Date() })
+        .where('ownedCouponId = :ownedCouponId', { ownedCouponId })
+        .execute();
     });
   }
 
